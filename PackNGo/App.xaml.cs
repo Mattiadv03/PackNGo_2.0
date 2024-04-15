00a74888e8f88956973d53b292164b8a3d6a214c
Mattiadv03 <mattiadallavalle03@gmail.com>
2024-04-15 12:20:43 +0200

    Aggiungere i file di progetto.

---
+++ PackNGo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PackNGo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}

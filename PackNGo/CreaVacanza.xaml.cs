d08dce03c2c935ddd606575e271b515913a21920
Mattiadv03 <mattiadallavalle03@gmail.com>
2024-04-15 15:42:02 +0200

    Aggiunti bottone per tornare alla MainPage

--- PackNGo/CreaVacanza.xaml.cs
+++ PackNGo/CreaVacanza.xaml.cs
@@ -5,5 +5,13 @@ public partial class CreaVacanza : ContentPage
 	public CreaVacanza()
 	{
 		InitializeComponent();
+
+        buttonTornaIndietro_CreaVacanza.Clicked += ButtonTornaIndietro_CreaVacanza_Clicked;
 	}
+
+    private void ButtonTornaIndietro_CreaVacanza_Clicked(object? sender, EventArgs e)
+    {
+        // Torno alla MainPage
+        Navigation.PushAsync(new MainPage());
+    }
 }
--- PackNGo/LeMieVacanze.xaml.cs
+++ PackNGo/LeMieVacanze.xaml.cs
@@ -5,5 +5,15 @@ public partial class LeMieVacanze : ContentPage
 	public LeMieVacanze()
 	{
 		InitializeComponent();
+
+        buttonTornaIndietro_LeMieVacanze.Clicked += ButtonTornaIndietro_LeMieVacanze_Clicked;
+
+
 	}
+
+    private void ButtonTornaIndietro_LeMieVacanze_Clicked(object? sender, EventArgs e)
+    {
+        // Torno alla MainPage
+        Navigation.PushAsync(new MainPage());
+    }
 }

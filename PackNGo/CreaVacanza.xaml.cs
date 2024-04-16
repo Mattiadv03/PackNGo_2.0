4848aea5497b762333cfeac37a216fa7fc034ed2
Mattiadv03 <mattiadallavalle03@gmail.com>
2024-04-16 09:59:24 +0200

    Aggiunta checkbox "Gita in giornata"

--- PackNGo/CreaVacanza.xaml.cs
+++ PackNGo/CreaVacanza.xaml.cs
@@ -7,7 +7,44 @@ public partial class CreaVacanza : ContentPage
 		InitializeComponent();
 
         buttonTornaIndietro_CreaVacanza.Clicked += ButtonTornaIndietro_CreaVacanza_Clicked;
+
+        sliderNumeroNotti_CreaVacanza.ValueChanged += SliderNumeroNotti_CreaVacanza_ValueChanged;
+
+
 	}
+
+    private void CheckboxGitaInGiornata_CheckedChanged(object? sender, CheckedChangedEventArgs e)
+    {
+        // Prendo lo stato corrente
+        bool isCheckboxGitaInGiornataChecked = checkboxGitaInGiornata_CreaVacanza.IsChecked;
+
+        if(isCheckboxGitaInGiornataChecked ) {
+            // True, disabilito lo slider
+            sliderNumeroNotti_CreaVacanza.IsEnabled = false;
+
+            // Nascondo la label delle notti
+            labelValoreSlider_CreaVacanza.IsVisible = false;
+        } else {
+            // Falso, abilito lo slider
+            sliderNumeroNotti_CreaVacanza.IsEnabled = true;
+
+            // Mostro la label delle notti
+            labelValoreSlider_CreaVacanza.IsVisible = true;
+        }
+    }
+
+    public void OnLabelTapped(object sender, TappedEventArgs args)
+    {
+        // Inverti lo stato della checkbox
+        checkboxGitaInGiornata_CreaVacanza.IsChecked = !checkboxGitaInGiornata_CreaVacanza.IsChecked;
+    }
+
+
+    private void SliderNumeroNotti_CreaVacanza_ValueChanged(object? sender, ValueChangedEventArgs e)
+    {
+        // Mostro il valore corrente
+        labelValoreSlider_CreaVacanza.Text = "La vacanza durerà " + Convert.ToString(Convert.ToInt32(sliderNumeroNotti_CreaVacanza.Value)) + " notti";
+    }
 
     private void ButtonTornaIndietro_CreaVacanza_Clicked(object? sender, EventArgs e)
     {

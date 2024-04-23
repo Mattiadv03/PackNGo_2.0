ab47626d200e538d04dbbb7415dd2a00bd74504c
= <=>
2024-04-23 15:38:33 +0200

    work in progress

--- PackNGo/ComponiValigia.xaml.cs
+++ PackNGo/ComponiValigia.xaml.cs
@@ -1,3 +1,6 @@
+
+using Newtonsoft.Json;
+
 namespace PackNGo;
 
 public partial class ComponiValigia : ContentPage
@@ -6,13 +9,13 @@ public partial class ComponiValigia : ContentPage
     int numeroNotti;
     string stagione;
     string tipologiaVacanza;
-    List<String> listaOptionalsScelti;
-
+    Dictionary<Label, CheckBox> listaOptionalsScelti;
+    Dictionary<Label, CheckBox> listaDefaultScelti;
     Dictionary<Label, CheckBox> listaElementi;
 
-    public ComponiValigia(string nomeVacanza, int numeroNotti, string stagione, string tipologiaVacanza, List<String> listaOptionalsScelti)
-	{
-		InitializeComponent();
+    public ComponiValigia(string nomeVacanza, int numeroNotti, string stagione, string tipologiaVacanza, Dictionary<Label, CheckBox> listaOptionalsScelti)
+    {
+        InitializeComponent();
 
         // Salvo i valori dal ScegliTipologia
         this.nomeVacanza = nomeVacanza;
@@ -20,9 +23,17 @@ public partial class ComponiValigia : ContentPage
         this.stagione = stagione;
         this.tipologiaVacanza = tipologiaVacanza;
         this.listaOptionalsScelti = listaOptionalsScelti;
-        
+
+        listaElementi = new Dictionary<Label, CheckBox>();
+
         // Imposto il titolo in base al nomeVacanza
         setTitolo();
+
+        // Popolo con gli elementi di default
+        popolaDefaultAsync();
+
+        // Popolo con gli optionals
+        popolaOptionals();
 
         // Seleziona/Deseleziona tutto
         buttonSelezioneDeseleziona_ComponiValigia.Clicked += ButtonSelezioneDeseleziona_ComponiValigia_Clicked;
@@ -31,16 +42,114 @@ public partial class ComponiValigia : ContentPage
         buttonTornaIndietro_ComponiValigia.Clicked += ButtonTornaIndietro_ComponiValigia_Clicked;
     }
 
-    private void ButtonSelezioneDeseleziona_ComponiValigia_Clicked(object? sender, EventArgs e)
+    private async Task popolaDefaultAsync()
     {
+        // Controllo se è connesso a internet
+        NetworkAccess accessType = Connectivity.Current.NetworkAccess;
+
+        if (accessType == NetworkAccess.Internet)
+        {
+            // Leggo da Github il file default.json
+            string urlJsonFile = "https://raw.githubusercontent.com/Mattiadv03/PackNGo_2.0/master/PackNGo/Resources/Json/default.json";
+
+            // Creo un'istanza di HttpClient
+            HttpClient client = new HttpClient();
+
+            // Scarico il file JSON dal'url
+            HttpResponseMessage response = await client.GetAsync(urlJsonFile);
+
+            if (response.IsSuccessStatusCode)
+            {
+                // Ottengo il corpo della risposta
+                string data = await response.Content.ReadAsStringAsync();
+
+                if (data is not null)
+                {
+                    Default listaDefault = JsonConvert.DeserializeObject<Default>(data);
+
+                    foreach (var item in listaDefault.GetType().GetProperties())
+                    {
+                        // Creo un StackLayout per allineare checkbox e label
+                        StackLayout stackLayoutDefault = new StackLayout
+                        {
+                            HorizontalOptions = LayoutOptions.CenterAndExpand,
+                            Orientation = StackOrientation.Horizontal
+                        };
+
+                        // Creo una checkbox
+                        CheckBox checkboxDefault = new CheckBox
+                        {
+                            HorizontalOptions = LayoutOptions.Center,
+                            VerticalOptions = LayoutOptions.Center,
+                            Color = Color.FromArgb("#ac99ea")
+                        };
+
+                        // Aggiungo all'HorizontalLayout
+                        stackLayoutDefault.Children.Add(checkboxDefault);
+
+                        // Prendo il nome dell'optional
+                        string nomeDefault = item.Name;
+
+                        // Creo la label associata
+                        Label labelOptional = new Label
+                        {
+                            Text = nomeDefault.Replace("_", " "),
+                            HorizontalOptions = LayoutOptions.Start,
+                            VerticalOptions = LayoutOptions.Center,
+                            GestureRecognizers =
+                            {
+                                new TapGestureRecognizer
+                                {
+                                    Command = new Command(() =>
+                                    {
+                                        checkboxDefault.IsChecked = !checkboxDefault.IsChecked;
+                                    })
+                                }
+                            }
+                        };
+
+                        // Aggiungo al dictionary
+                        listaElementi.Add(labelOptional, checkboxDefault);
+
+                        // Aggiungo all'StackLayout
+                        stackLayoutDefault.Children.Add(checkboxDefault);
+
+                        // Aggiungo l'HorizontalLayout al FlexLayout
+                        stackLayoutDefault.Children.Add(checkboxDefault);
+                    }
+
+                }
+                else
+                {
+                    await DisplayAlert("Errore", "Errore durante la lettura del file", "OK");
+                }
+            }
+            else
+            {
+                await DisplayAlert("Errore", "File non trovato, controlla il repository su Github", "OK");
+            }
+        }
+        else
+        {
+            await DisplayAlert("Errore", "Connettiti a internet per utilizzare l'applicazione", "OK");
+        }
+    }
+
+    private void popolaOptionals()
+    {
+
+    }
+
+    private void ButtonSelezioneDeseleziona_ComponiValigia_Clicked(object? sender, EventArgs e)
+    { 
         if(buttonSelezioneDeseleziona_ComponiValigia.Text == "Seleziona tutto")
         {
             // Rendo IsChecked a true per ogni elemento
-            foreach (var elemento in listaOptionalsScelti)
+            foreach (var elemento in listaElementi)
             {
-                if (!listaOptionalsScelti[elemento.Key].IsChecked)
+                if (!listaElementi[elemento.Key].IsChecked)
                 {
-                    listaOptionalsScelti[elemento.Key].IsChecked = true;
+                    listaElementi[elemento.Key].IsChecked = true;
                 }
             }
 
@@ -50,11 +159,11 @@ public partial class ComponiValigia : ContentPage
         else
         {
             // Rendo IsChecked a true per ogni elemento
-            foreach (var elemento in listaOptionalsScelti)
+            foreach (var elemento in listaElementi)
             {
-                if (listaOptionalsScelti[elemento.Key].IsChecked)
+                if (listaElementi[elemento.Key].IsChecked)
                 {
-                    listaOptionalsScelti[elemento.Key].IsChecked = false;
+                    listaElementi[elemento.Key].IsChecked = false;
                 }
             }
 
---
+++ PackNGo/Default.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackNGo
+{
+    public class Beauty
+    {
+        public List<string> Denti { get; set; }
+        public List<string> Doccia { get; set; }
+
+        [JsonProperty("Cotton fioc")]
+        public string Cottonfioc { get; set; }
+        public string Cera { get; set; }
+        public List<string> Cerotti { get; set; }
+        public List<string> Extra { get; set; }
+    }
+
+    public class Intimo
+    {
+        public string Mutande { get; set; }
+        public string Calzini { get; set; }
+        public List<string> Pigiama { get; set; }
+
+        [JsonProperty("Borsetta roba onta intimo")]
+        public int Borsettarobaontaintimo { get; set; }
+    }
+
+    public class Default
+    {
+        public Beauty Beauty { get; set; }
+        public List<string> Elettronica { get; set; }
+        public List<string> Utility { get; set; }
+        public Intimo Intimo { get; set; }
+    }
+
+
+}
